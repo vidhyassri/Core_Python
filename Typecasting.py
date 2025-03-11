12daa83638796a0991b0088545ca42ad14a93ca6
vidhyassri <vidhyassrik@gmail.com>
2025-03-11 17:46:22 +0530

    casting and operator

---
+++ Typecasting.py
@@ -0,0 +1,11 @@
+yet=44555
+print(yet,type(yet))
+fit=float(yet)
+print(fit)
+print(fit-30)
+
+
+
+income=float(input("enter amt"))
+print(income,type(income))
+print(int(income))
---
+++ operator/arithmetic.py
@@ -0,0 +1,27 @@
+# value=1626     #Delare the variable
+# value1=2701
+# result=0
+# result=value+value1
+# print("addition;",result)
+# result=value-value1
+# print("subraction;",result)
+# result=value*value1
+# print("multilication;",result)
+# result=value/value1
+# print("division;",result)
+# result=value%value1
+# print("modulus;",result)
+
+
+'''add=int(input("enter the value"))
+add1=int(input("enter the value"))
+sum=add+add1
+print("addition;",sum)'''
+
+
+
+orginal_price=1200
+discount_percentage=15
+discount_amt=(orginal_price*discount_percentage)/100
+final_price=orginal_price-discount_amt
+print("discounted price:",(final_price))
